c1fc0bebb397ce60ac78f164fe33bef769b2a9b9
Mathieu <mathieu.gilardet@gmail.com>
2020-05-14 17:08:29 +0200

    setup project (intellij)

--- src/AirMonster.java
+++ src/AirMonster.java
@@ -3,5 +3,6 @@ class AirMonster {
     // TODO : add constructor and initialize type to "air"
     
     // TODO : create an attack method which deals x2 damage to "water"
-    
+ 
+
 }
--- src/Arena.java
+++ src/Arena.java
@@ -1,7 +1,7 @@
 class Arena {
 
     public static void main(String[] args) {
-        
+  	    System.out.println("start");  
         // TODO : create two monsters
 
         // TODO : monsters attack each other
